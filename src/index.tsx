919425bc76168ca0268190b91cddd768cf4baad7
Vasil Vanchuk <v.vanchuk@tinkoff.ru>
2024-11-04 22:02:34 +0300

    Define basename for router

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="react-knitting">
       <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<SockInput />} />
