e4fa23cb0949d591a4201d48a8284d430676237f
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-11-04 19:24:46 +0300

    fix dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+//const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+//const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
